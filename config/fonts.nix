6a1a663c32008e5dd2c64044529df375e5b7aaf7
Tommy Bidne <tbidne@protonmail.com>
2021-01-12 23:13:11 -0500

    Add ringbearer font

--- config/fonts.nix
+++ config/fonts.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }:
 
+let
+  ringbearerRepo = builtins.fetchGit {
+    url = "https://github.com/tbidne/ringbearer.git";
+    ref = "main";
+    rev = "10e60077b49bf36765eada7b4757b8b45fbd98a9";
+  };
+  ringbearer = pkgs.callPackage ringbearerRepo {};
+in
 {
   fonts.fonts = with pkgs;
     [ hasklig
+      ringbearer
     ];
 }
