76c3dad96cb31635928f55b84237f3cb6d0625ca
Tommy Bidne <tbidne@protonmail.com>
2021-01-22 23:12:12 -0500

    Add sddm back, remove failed hidpi fix for desktop cursor :-(

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,11 +14,4 @@
     ];
 
   nixpkgs.config.allowUnfree = true;
-
-  # To make mouse pointer larger on HiDPI
-  xsession.pointerCursor = {
-    package = pkgs.vanilla-dmz;
-    name = "Vanilla-DMZ";
-    size = 128;
-  };
 }
