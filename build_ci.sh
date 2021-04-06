146a6025aaceb14a92373b282b18cc6a1aa5920a
Tommy Bidne <tbidne@protonmail.com>
2021-04-07 08:37:17 +1200

    Fix CI take 2

--- build_ci.sh
+++ build_ci.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nix-shell -E 'with import <nixpkgs> {}; stdenv.mkDerivation { name = "try-build"; buildInputs = [ (nixos {}).nixos-rebuild ]; }' --command "nixos-rebuild dry-build"
+nix-shell -E 'with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ad47284f8b01f587e24a4f14e0f93126d8ebecda.tar.gz") {}; stdenv.mkDerivation { name = "try-build"; buildInputs = [ (nixos {}).nixos-rebuild ]; }' --command "nixos-rebuild dry-build"
