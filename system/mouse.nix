d7b6bbbda9e4f9f8a59c9461df0939226d5f79b3
Tommy Bidne <tbidne@protonmail.com>
2021-04-07 08:39:17 +1200

    Remove mouse settings

--- system/mouse.nix
+++ system/mouse.nix
@@ -7,12 +7,4 @@
     naturalScrolling = true;
     tapping = true;
   };
-
-  services.xserver.libinput.mouse = {
-    # -1.0 to 1.0, helps touchpad accel on HiDPI
-    accelSpeed = "0.5";
-    middleEmulation = true;
-    naturalScrolling = true;
-    tapping = true;
-  };
 }
