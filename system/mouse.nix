6799dfc8561ac3b26608686d43cdd90e481461fc
Tommy Bidne <tbidne@protonmail.com>
2021-04-08 10:28:30 +1200

    Add (broken) natural scrolling to mouse

--- system/mouse.nix
+++ system/mouse.nix
@@ -1,10 +1,19 @@
 {
-  # touchpad
-  services.xserver.libinput.touchpad = {
-    # -1.0 to 1.0, helps touchpad accel on HiDPI
-    accelSpeed = "0.5";
-    middleEmulation = true;
-    naturalScrolling = true;
-    tapping = true;
+  services.xserver.libinput = {
+    enable = true;
+
+    mouse = {
+      # This doesn't seem to work, but this is probably what we eventually
+      # want, so leaving it for now.
+      naturalScrolling = true;
+    };
+
+    touchpad = {
+      # -1.0 to 1.0, helps touchpad accel on HiDPI
+      accelSpeed = "0.5";
+      middleEmulation = true;
+      naturalScrolling = true;
+      tapping = true;
+    };
   };
 }
