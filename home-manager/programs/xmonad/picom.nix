0830e01d01d8d2b229bcdb19a5738cac60764df7
Tommy Bidne <tbidne@protonmail.com>
2021-06-14 15:11:53 +1200

    Revert git to vscode, add explanation to picom

--- home-manager/programs/xmonad/picom.nix
+++ home-manager/programs/xmonad/picom.nix
@@ -1,4 +1,13 @@
 {
+  # NOTE: The opacity settings (at least) don't appear to be picked up.
+  # They are correctly written to the conf files in /nix/store, but they
+  # do not seem to have an effect. The culprit may be XMonad (and ewmh?),
+  # so for now opacity is handled individually, e.g. kitty has its own
+  # settings, and XMonad handles the general "inactive = faded" setting.
+  #
+  # I discovered this when trying to add blurring here, which also does
+  # not work, sadly. Keeping this here for now, but we may have to switch
+  # WMs to get blurring/rounded corners.
   services.picom = {
     enable = true;
     activeOpacity = "1.0";
