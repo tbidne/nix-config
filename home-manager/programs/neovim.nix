33cbb2b2dd30b14f339cd8ef2fed94fbbb8f22f6
Tommy Bidne <tbidne@protonmail.com>
2021-06-29 13:58:14 +1200

    Neovim updates
    1. Disable theme (looks better w/ transparency)
    2. Swap smartindent for vim-sleuth (works better)
    3. Add show whitespace command
    4. Add lsp shortcuts

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -5,17 +5,24 @@
 
     extraConfig = ''
       colorscheme nord
-      set termguicolors
+      "set termguicolors
       let g:airline_theme='base16_nord'
 
       let NERDTreeShowHidden=1
-      set smartindent
+      
+      "set whitespace with :set list
+      :set listchars=eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:·
 
       nmap <C-p> :FZF<CR>
       nmap <C-t> :NERDTreeToggle<CR>
       nmap <Tab> <C-w>w
-      nmap <C-h> :call CocAction('doHover')<CR>
       nmap <C-b> :ToggleBufExplorer<CR>
+      nmap <C-s> :set list!<CR>
+
+      "CoC Actions
+      nmap <C-f> :call CocAction('format')<CR>
+      nmap <C-h> :call CocAction('doHover')<CR>
+      nmap <C-j> :call CocAction('jumpDefinition')<CR>
     '';
 
     plugins = with pkgs.vimPlugins; [
@@ -34,6 +41,7 @@
       vim-hoogle
       vim-lsp
       vim-nix
+      vim-sleuth
     ];
   };
 
