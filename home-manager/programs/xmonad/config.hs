439122cfdea9efac3b7adcc52ea13325976885cb
Tommy Bidne <tbidne@protonmail.com>
2021-07-19 00:04:47 +1200

    Swap paths back to uppercase

--- home-manager/programs/xmonad/config.hs
+++ home-manager/programs/xmonad/config.hs
@@ -49,7 +49,7 @@ main = mkDbusClient >>= withDBus
 
 withDBus :: Client -> IO ()
 withDBus dbus = do
-  XWallpaper.setRandomWallpaper ["$HOME/pictures/wallpaper/current"]
+  XWallpaper.setRandomWallpaper ["$HOME/Pictures/wallpaper/current"]
   let config =
         keybindings $
           X.def
