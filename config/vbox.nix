c531a328534db692cd23a3668bf8332c13d694e4
Tommy Bidne <tbidne@protonmail.com>
2021-07-24 11:05:07 +1200

    Remove guest settings from vbox

--- config/vbox.nix
+++ config/vbox.nix
@@ -1,6 +1,8 @@
 {
-   virtualisation.virtualbox.host.enable = true;
-   users.extraGroups.vboxusers.members = [ "tommy" ];
-   virtualisation.virtualbox.host.enableExtensionPack = true;
-   virtualisation.virtualbox.guest.enable = true;
+   virtualisation.virtualbox = {
+      host = {
+         enable = true;
+         enableExtensionPack = true;
+      };
+   };
 }
