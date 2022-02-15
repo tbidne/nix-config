b7e807cf3f3f2c2fa0ebee2cea6d85f29dc33e95
Tommy Bidne <tbidne@protonmail.com>
2022-02-15 18:08:28 +1300

    Downgrade nixpkgs, home-manager until bug is fixed

--- config/cache.nix
+++ config/cache.nix
@@ -1,21 +1,22 @@
 {
-  nix.settings = {
-    substituters = [
-      "https://nixcache.reflex-frp.org"
-      "https://cache.nixos.org"
-      "https://shpadoinkle.cachix.org"
-      "https://hydra.iohk.io"
-      "https://iohk.cachix.org"
-      "ssh://nix-ssh@ci.ardana.platonic.systems"
-    ];
+  # Swap these once we can upgrade nixpkgs
+  #nix.settings.substituters = [
+  nix.binaryCaches = [
+    "https://nixcache.reflex-frp.org"
+    "https://cache.nixos.org"
+    "https://shpadoinkle.cachix.org"
+    "https://hydra.iohk.io"
+    "https://iohk.cachix.org"
+    "ssh://nix-ssh@ci.ardana.platonic.systems"
+  ];
 
-    trusted-public-keys = [
-      "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "shpadoinkle.cachix.org-1:aRltE7Yto3ArhZyVjsyqWh1hmcCf27pYSmO1dPaadZ8="
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
-      "ci.ardana.platonic.systems:yByqhxfJ9KIUOyiCe3FYhV7GMysJSA3i5JRvgPuySsI="
-    ];
-  };
+  #nix.settings.trusted-public-keys = [
+  nix.binaryCachePublicKeys = [
+    "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "shpadoinkle.cachix.org-1:aRltE7Yto3ArhZyVjsyqWh1hmcCf27pYSmO1dPaadZ8="
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+    "ci.ardana.platonic.systems:yByqhxfJ9KIUOyiCe3FYhV7GMysJSA3i5JRvgPuySsI="
+  ];
 }
--- flake.nix
+++ flake.nix
@@ -3,11 +3,14 @@
 
   inputs = {
     # nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Downgrade nixpkgs and home-manager until NixOS/nixpkgs/pull/159255 is
+    # merged. See https://nixpk.gs/pr-tracker.html?pr=159255.
+    nixpkgs.url = "github:nixos/nixpkgs?rev=689b76bcf36055afdeb2e9852f5ecdd2bf483f87";
     my-nixpkgs.url = "github:tbidne/nixpkgs/vscode-exts";
 
     # utils
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager?rev=acf824c9ed70f623b424c2ca41d0f6821014c67c";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nur.url = "github:nix-community/NUR";
     flake-utils.url = "github:numtide/flake-utils";
 
