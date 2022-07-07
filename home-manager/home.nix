69f63a7c21f9bc97f549a24603d250d4508481bf
Tommy Bidne <tbidne@protonmail.com>
2022-07-08 09:50:29 +1200

    Fix outdated caches being used

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,4 +4,15 @@
     (import ./programs/default.nix { inherit inputs; })
     (import ./system/default.nix { inherit inputs; })
   ];
+
+  home.file = {
+    # Blanking these files. I'm not sure where they came from (they are not
+    # part of nixos-rebuild), but they were holding outdated cache info that
+    # was causing my system to use caches not reflected in
+    # nix.settings.substituters.
+    #
+    # A better solution would be to prevent these files from being created.
+    ".config/nix/netrc".text = "";
+    ".config/nix/nix.conf".text = "";
+  };
 }
