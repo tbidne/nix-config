bf0bd45ccdd284890a08f9d586c241d35ace4397
Tommy Bidne <tbidne@protonmail.com>
2022-11-27 12:14:13 +1300

    Improve plasma config

--- home-manager/system/plasma/default.nix
+++ home-manager/system/plasma/default.nix
@@ -12,13 +12,28 @@
   # 4. diff old.nix new.nix
   # Between the diff, github:pjones/plasma-manager, and files themselves
   # (e.g. kglobalshortcutsrc), modify the below settings.
+  #
+  # Naturally, we need to reload the ui to see any changes.
   programs.plasma = {
     enable = true;
+
+    hotkeys.commands."Kitty Dropdown" = {
+      key = "Alt+=";
+      command = "tdrop -h 40% kitty";
+    };
 
     shortcuts = {
       kwin = {
         ShowDesktopGrid = [ "Meta+Ctrl+Space" "Ctrl+F8" ];
         "Window Close" = [ "Alt+F4" "Meta+Backspace" ];
+        "Window to Desktop 1" = "Alt+1";
+        "Window to Desktop 2" = "Alt+2";
+        "Window to Desktop 3" = "Alt+3";
+        "Window to Desktop 4" = "Alt+4";
+        "Window to Desktop 5" = "Alt+5";
+        "Window to Desktop 6" = "Alt+6";
+        "Window to Desktop 7" = "Alt+7";
+        "Window to Desktop 8" = "Alt+8";
       };
       # This allows us to use meta to open the start menu
       "plasmashell"."activate widget 23" = "Alt+F1,none,Activate Application Launcher Widget";
@@ -26,6 +41,7 @@
     # TODO: Active windows should steal focus e.g. Ctrl+o in vscode should be
     # focused, but it is not.
     files = {
+      #"dolphinsrc"."IconsMode"."PreviewSize" = 128;
       "kwinrc"."Desktops"."Id_1" = "3868ba12-ce3b-4f2a-838a-c79089819633";
       "kwinrc"."Desktops"."Id_2" = "419c8acf-d73b-48a8-a244-a748545c7674";
       "kwinrc"."Desktops"."Id_3" = "51eb43ac-377f-493f-900e-185583f81ad7";
@@ -47,32 +63,29 @@
       "kcminputrc"."Mouse"."XLbInptNaturalScroll" = true;
       "kcminputrc"."Mouse"."cursorSize" = 48;
       "kwinrc"."Windows"."FocusPolicy" = "FocusFollowsMouse";
-      "khotkeysrc"."Data"."DataCount" = 1;
-      "khotkeysrc"."Data_1"."Comment" = "Dropdown functionality for kitty";
-      "khotkeysrc"."Data_1"."Enabled" = true;
-      "khotkeysrc"."Data_1"."Name" = "Kitty Dropdown";
-      "khotkeysrc"."Data_1"."Type" = "SIMPLE_ACTION_DATA";
-      "khotkeysrc"."Data_1Actions"."ActionsCount" = 1;
-      "khotkeysrc"."Data_1Actions0"."CommandURL" = "tdrop -h 40% kitty";
-      "khotkeysrc"."Data_1Actions0"."Type" = "COMMAND_URL";
-      "khotkeysrc"."Data_1Conditions"."Comment" = "";
-      "khotkeysrc"."Data_1Conditions"."ConditionsCount" = 0;
-      "khotkeysrc"."Data_1Triggers"."Comment" = "Simple_action";
-      "khotkeysrc"."Data_1Triggers"."TriggersCount" = 1;
-      "khotkeysrc"."Data_1Triggers0"."Key" = "Meta+Ctrl+=";
-      "khotkeysrc"."Data_1Triggers0"."Type" = "SHORTCUT";
-      "khotkeysrc"."Data_1Triggers0"."Uuid" = "{f8479688-04b8-4868-82c5-9748d5e4a736}";
-      "kwinrulesrc"."61671be3-2d06-43c4-bb44-77664eb4c98c"."Description" = "Window settings for kitty";
-      "kwinrulesrc"."61671be3-2d06-43c4-bb44-77664eb4c98c"."clientmachine" = "localhost";
-      "kwinrulesrc"."61671be3-2d06-43c4-bb44-77664eb4c98c"."opacityactive" = 90;
-      "kwinrulesrc"."61671be3-2d06-43c4-bb44-77664eb4c98c"."opacityactiverule" = 2;
-      "kwinrulesrc"."61671be3-2d06-43c4-bb44-77664eb4c98c"."title" = "bash";
-      "kwinrulesrc"."61671be3-2d06-43c4-bb44-77664eb4c98c"."titlematch" = 1;
-      "kwinrulesrc"."61671be3-2d06-43c4-bb44-77664eb4c98c"."types" = 1;
-      "kwinrulesrc"."61671be3-2d06-43c4-bb44-77664eb4c98c"."wmclass" = "kitty";
-      "kwinrulesrc"."61671be3-2d06-43c4-bb44-77664eb4c98c"."wmclassmatch" = 1;
-      "kwinrulesrc"."General"."count" = 1;
-      "kwinrulesrc"."General"."rules" = "61671be3-2d06-43c4-bb44-77664eb4c98c";
+      "kwinrulesrc"."1"."Description" = "Window settings for kitty";
+      "kwinrulesrc"."1"."clientmachine" = "localhost";
+      "kwinrulesrc"."1"."noborder" = true;
+      "kwinrulesrc"."1"."noborderrule" = 3;
+      "kwinrulesrc"."1"."opacityactive" = 95;
+      "kwinrulesrc"."1"."opacityactiverule" = 2;
+      "kwinrulesrc"."1"."title" = "bash";
+      "kwinrulesrc"."1"."titlematch" = 1;
+      "kwinrulesrc"."1"."types" = 1;
+      "kwinrulesrc"."1"."wmclass" = "kitty";
+      "kwinrulesrc"."1"."wmclassmatch" = 1;
+      "kwinrulesrc"."2"."Description" = "Window settings for vscodium";
+      "kwinrulesrc"."2"."clientmachine" = "localhost";
+      "kwinrulesrc"."2"."noborder" = true;
+      "kwinrulesrc"."2"."noborderrule" = 3;
+      "kwinrulesrc"."2"."title" = "VSCodium";
+      "kwinrulesrc"."2"."types" = 1;
+      "kwinrulesrc"."2"."wmclass" = "vscodium";
+      "kwinrulesrc"."2"."wmclassmatch" = 1;
+      "kwinrulesrc"."2"."windowrole" = "browser-window";
+      "kwinrulesrc"."2"."windowrolematch" = 1;
+      "kwinrulesrc"."General"."count" = 2;
+      "kwinrulesrc"."General"."rules" = "1,2";
     };
   };
 }
