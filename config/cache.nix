fa09f324682500e0f7c985df23abfa4965e228fb
Tommy Bidne <tbidne@protonmail.com>
2022-12-04 09:45:53 +1300

    Update iog cache

--- config/cache.nix
+++ config/cache.nix
@@ -2,17 +2,16 @@
   nix.settings.substituters = [
     "https://nixcache.reflex-frp.org"
     "https://cache.nixos.org"
-    "https://hydra.iohk.io"
-    "https://iohk.cachix.org"
+    "https://cache.iog.io"
   ];
 
   nix.settings.trusted-public-keys = [
     "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
   ];
 
-  # don't fail if cache is down
+  # Don't fail if cache is down. This does not seem to work reliably.
+  # See: https://github.com/NixOS/nix/issues/3514
   nix.settings.fallback = true;
 }
