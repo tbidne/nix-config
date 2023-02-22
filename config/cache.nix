715ef2e524784d511cadcd544b491711f552dbe8
Tommy Bidne <tbidne@protonmail.com>
2023-02-22 14:28:37 +1300

    Add cache

--- config/cache.nix
+++ config/cache.nix
@@ -3,12 +3,16 @@
     "https://nixcache.reflex-frp.org"
     "https://cache.nixos.org"
     "https://cache.iog.io"
+    # Added for haskell.nix as IOG's was not working for ghc 9.4.4
+    # See https://github.com/input-output-hk/haskell.nix/issues/1824
+    "https://cache.zw3rk.com"
   ];
 
   nix.settings.trusted-public-keys = [
     "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
   ];
 
   # Don't fail if cache is down. This does not seem to work reliably.
