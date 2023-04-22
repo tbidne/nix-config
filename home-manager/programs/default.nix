950455c207a7a085d5c89e50ded0a9729ed7e23a
Tommy Bidne <tbidne@protonmail.com>
2023-04-22 15:04:16 +1200

    Update time-conv

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -9,6 +9,7 @@
     (import ./kitty/default.nix { inherit inputs; })
     (import ./navi/default.nix { inherit inputs; })
     ./safe-rm/default.nix
+    ./time-conv/default.nix
     (import ./shrun/default.nix { inherit inputs; })
     (import ./vscode.nix { inherit inputs; })
   ];
---
+++ home-manager/programs/time-conv/default.nix
@@ -0,0 +1,5 @@
+{
+  home.file.".config/time-conv/config.toml" = {
+    source = ./config.toml;
+  };
+}
