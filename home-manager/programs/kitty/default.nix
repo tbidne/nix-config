b121b9b65a64b2d233097fd9fbd738be5d25919c
Tommy Bidne <tbidne@protonmail.com>
2023-05-15 09:41:54 +1200

    Fix kitty cursor

--- home-manager/programs/kitty/default.nix
+++ home-manager/programs/kitty/default.nix
@@ -8,11 +8,15 @@ in
     enable = true;
     font.name = "hasklig";
     settings = {
-      shell = "/run/current-system/sw/bin/bash";
-      wheel_scroll_multiplier = 1;
-      touch_scroll_multiplier = 1;
+      cursor_shape = "block";
       enable_audio_bell = false;
+      shell = "/run/current-system/sw/bin/bash";
+      # needed so that we use the cursor_shape block. otherwise it is overridden
+      # somehow.
+      shell_integration = "no-cursor";
       tab_bar_style = "powerline";
+      touch_scroll_multiplier = 1;
+      wheel_scroll_multiplier = 1;
     };
     extraConfig = ''
       ${theme}
