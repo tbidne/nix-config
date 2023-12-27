bfc9d990298a0508cf2aea135df66e2ae175e04f
Tommy Bidne <tbidne@protonmail.com>
2023-12-27 17:23:53 +1300

    Add more git aliases

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -26,6 +26,7 @@
       count = "count-objects -vH";
       count-commits = "rev-list --count";
       ff = "merge @{u} --ff-only";
+      m = "merge --ff-only";
       ft = "fetch --prune";
       l = "log";
       log-me = "log --author='Tommy Bidne'";
@@ -36,11 +37,36 @@
       lol = "log --oneline";
       redo = "commit --amend --no-edit";
 
+      # r = <remote>
+      # u = upstream
+      # b = <branch>
+      # c = $current_branch
+
       # fast-forward param remote + param branch
       ffrb = "!f() { git fetch $1 --prune; git merge $1/$2 --ff-only; }; f";
 
       # fast-forward fixed remote 'upstream' + param branch
       ffub = "!f() { git fetch upstream --prune; git merge upstream/$1 --ff-only; }; f";
+
+      # fast-forward fixed remote 'upstream' + current branch
+      ffuc = ''
+      !f() {
+        curr_branch=$(git rev-parse --abbrev-ref HEAD)
+        git fetch upstream --prune
+        git merge upstream/$curr_branch --ff-only
+      }
+      f
+      '';
+
+      # fast-forward fixed param remote + current branch
+      ffrc = ''
+      !f() {
+        curr_branch=$(git rev-parse --abbrev-ref HEAD)
+        git fetch $1 --prune
+        git merge $1/$curr_branch --ff-only
+      }
+      f
+      '';
 
       # e.g. git log-date "2021-07-27 12:00" "master"
       log-date = "!f() { git rev-list -n 1 --before=\"$1\" $2; }; f";
