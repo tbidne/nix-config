f0ed0c4e0e054cf87495f8198a7e8ff31dd3dd1b
Tommy Bidne <tbidne@protonmail.com>
2024-09-16 19:55:20 +1200

    Add ghc nix, remove zw3rk

--- config/cache.nix
+++ config/cache.nix
@@ -3,16 +3,14 @@
     "https://nixcache.reflex-frp.org"
     "https://cache.nixos.org"
     "https://cache.iog.io"
-    # Added for haskell.nix as IOG's was not working for ghc 9.4.4
-    # See https://github.com/input-output-hk/haskell.nix/issues/1824
-    "https://cache.zw3rk.com"
+    "https://ghc-nix.cachix.org"
   ];
 
   nix.settings.trusted-public-keys = [
     "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+    "ghc-nix.cachix.org-1:wI8l3tirheIpjRnr2OZh6YXXNdK2fVQeOI4SVz/X8nA="
   ];
 
   # Don't fail if cache is down. This does not seem to work reliably.
