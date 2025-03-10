5863d032e6e9b5bccb8d5f66e791677c5d7c0e5c
Tommy Bidne <tbidne@protonmail.com>
2025-03-10 15:20:39 +1300

    Update inputs

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -167,148 +167,152 @@ in
     # it is ~/.vscode/.
     package = inputs.pkgs.vscodium;
 
-    # BUG: Encountered a strange bug where none of my extensions loaded.
-    # First, to test changes, we probably need to make a change to the
-    # extension set here i.e. enable/disable some.
-    #
-    # To be honest, I have no idea what caused it. I deleted .vscode,
-    # .vscode-oss, and ~/.VSCodium. I also toggled the nix setting
-    #
-    #   mutableExtensionsDir = true;
-    #
-    # (default true, I think). In the end, what worked __appears__ to be the
-    # combination of deleting those directories and rebuilding, ensuring
-    # we make some changes to the extension list so that a rebuild actually
-    # happens.
+    profiles.default = {
 
-    # If this worked then we should _at least_ have extensions in
-    # .vscode-oss/extensions/extensions.json
-    extensions =
-      with inputs.pkgs.vscode-extensions;
-      [
-        arcticicestudio.nord-visual-studio-code
-        bbenoist.nix
-        bungcip.better-toml
-        dracula-theme.theme-dracula
-        eamodio.gitlens
-        editorconfig.editorconfig
-        haskell.haskell
-        james-yu.latex-workshop
-        jnoortheen.nix-ide
-        justusadam.language-haskell
-        mechatroner.rainbow-csv
-        #ms-python.python # FIXME: currently broken
-        ms-python.vscode-pylance
-        octref.vetur
-        rust-lang.rust-analyzer
-      ]
-      ++ externalExts;
+      # BUG: Encountered a strange bug where none of my extensions loaded.
+      # First, to test changes, we probably need to make a change to the
+      # extension set here i.e. enable/disable some.
+      #
+      # To be honest, I have no idea what caused it. I deleted .vscode,
+      # .vscode-oss, and ~/.VSCodium. I also toggled the nix setting
+      #
+      #   mutableExtensionsDir = true;
+      #
+      # (default true, I think). In the end, what worked __appears__ to be the
+      # combination of deleting those directories and rebuilding, ensuring
+      # we make some changes to the extension list so that a rebuild actually
+      # happens.
 
-    globalSnippets = {
-      texMathBbC = {
-        body = [ "ℂ" ];
-        description = "Insert LaTeX \\mathbb{C}, U+2112";
-        prefix = [ "\\C" ];
-      };
-      texMathBbN = {
-        body = [ "ℕ" ];
-        description = "Insert LaTeX \\mathbb{N}, U+2115";
-        prefix = [ "\\N" ];
-      };
-      texMathBbP = {
-        body = [ "ℙ" ];
-        description = "Insert LaTeX \\mathbb{P} U+2119";
-        prefix = [ "\\P" ];
-      };
-      texMathBbQ = {
-        body = [ "ℚ" ];
-        description = "Insert LaTeX \\mathbb{Q}, U+211A";
-        prefix = [ "\\Q" ];
-      };
-      texMathBbR = {
-        body = [ "ℝ" ];
-        description = "Insert LaTeX \\mathbb{R}, U+211D";
-        prefix = [ "\\R" ];
-      };
-      texMathBbZ = {
-        body = [ "ℤ" ];
-        description = "Insert LaTeX \\mathbb{Z}, U+2114";
-        prefix = [ "\\Z" ];
-      };
-    };
+      # If this worked then we should _at least_ have extensions in
+      # .vscode-oss/extensions/extensions.json
+      extensions =
+        with inputs.pkgs.vscode-extensions;
+        [
+          arcticicestudio.nord-visual-studio-code
+          bbenoist.nix
+          bungcip.better-toml
+          dracula-theme.theme-dracula
+          eamodio.gitlens
+          editorconfig.editorconfig
+          haskell.haskell
+          james-yu.latex-workshop
+          jnoortheen.nix-ide
+          justusadam.language-haskell
+          mechatroner.rainbow-csv
+          #ms-python.python # FIXME: currently broken
+          ms-python.vscode-pylance
+          octref.vetur
+          rust-lang.rust-analyzer
+        ]
+        ++ externalExts;
 
-    userSettings = {
-      "breadcrumbs.enabled" = true;
-      "editor.fontLigatures" = true;
-      "editor.fontFamily" = "Hasklig, Menlo, Menlo, Monaco, 'Courier New', monospace";
-      "editor.minimap.enabled" = false;
-      "editor.renderWhitespace" = "boundary";
-      "editor.rulers" = [ 80 ];
-      "editor.tabSize" = 2;
-      "editor.wordWrap" = "on";
-      "editor.wordWrapColumn" = 80;
-      "editor.bracketPairColorization.enabled" = true;
-      "editor.guides.bracketPairs" = "active";
-      "editor.unicodeHighlight.ambiguousCharacters" = false;
-
-      # so e.g. format json files does not strip final newline.
-      "files.insertFinalNewline" = true;
-
-      "todohighlight.defaultStyle" = {
-        "backgroundColor" = "none";
-        "fontWeight" = "bold";
+      globalSnippets = {
+        texMathBbC = {
+          body = [ "ℂ" ];
+          description = "Insert LaTeX \\mathbb{C}, U+2112";
+          prefix = [ "\\C" ];
+        };
+        texMathBbN = {
+          body = [ "ℕ" ];
+          description = "Insert LaTeX \\mathbb{N}, U+2115";
+          prefix = [ "\\N" ];
+        };
+        texMathBbP = {
+          body = [ "ℙ" ];
+          description = "Insert LaTeX \\mathbb{P} U+2119";
+          prefix = [ "\\P" ];
+        };
+        texMathBbQ = {
+          body = [ "ℚ" ];
+          description = "Insert LaTeX \\mathbb{Q}, U+211A";
+          prefix = [ "\\Q" ];
+        };
+        texMathBbR = {
+          body = [ "ℝ" ];
+          description = "Insert LaTeX \\mathbb{R}, U+211D";
+          prefix = [ "\\R" ];
+        };
+        texMathBbZ = {
+          body = [ "ℤ" ];
+          description = "Insert LaTeX \\mathbb{Z}, U+2114";
+          prefix = [ "\\Z" ];
+        };
       };
-      # HACK: Whitespace on keywords w/o colons is because they occasionally
-      # can appear as part of another word (e.g. DEBUG), and we do not want
-      # highlighting to apply in those situations.
-      "todohighlight.keywords" = [
-        {
-          "text" = "BUG:";
-          "color" = "#ff6c6b";
-        }
-        {
-          "text" = "DEPRECATED:";
-          "color" = "#83898d";
-        }
-        {
-          "text" = "FIXME:";
-          "color" = "#ff6c6b";
+
+      userSettings = {
+        "breadcrumbs.enabled" = true;
+        "editor.fontLigatures" = true;
+        "editor.fontFamily" = "Hasklig, Menlo, Menlo, Monaco, 'Courier New', monospace";
+        "editor.minimap.enabled" = false;
+        "editor.renderWhitespace" = "boundary";
+        "editor.rulers" = [ 80 ];
+        "editor.tabSize" = 2;
+        "editor.wordWrap" = "on";
+        "editor.wordWrapColumn" = 80;
+        "editor.bracketPairColorization.enabled" = true;
+        "editor.guides.bracketPairs" = "active";
+        "editor.unicodeHighlight.ambiguousCharacters" = false;
+
+        # so e.g. format json files does not strip final newline.
+        "files.insertFinalNewline" = true;
+
+        "todohighlight.defaultStyle" = {
           "backgroundColor" = "none";
-        }
-        {
-          "text" = "HACK:";
-          "color" = "#a9a1e1";
-        }
-        {
-          "text" = "NOTE:";
-          "color" = "#5ea164";
-        }
-        {
-          "text" = "REVIEW:";
-          "color" = "#51afef";
-        }
-        {
-          "text" = "TODO:";
-          "color" = "#ecbe7b";
-          # override default
-          "backgroundColor" = "none";
-        }
-      ];
-      "update.mode" = "manual";
-      "window.menuBarVisibility" = "toggle";
-      # This options seems deprecated
-      #"workbench.activityBar.location" = "hidden";
-      "workbench.colorTheme" = "Tokyo Night";
-      "workbench.colorCustomizations" = {
-        "activityBar.background" = "#242631";
-        "editorGroupHeader.tabsBackground" = "#242631";
-        "statusBar.background" = "#242631";
-        "titleBar.activeBackground" = "#242631";
-        "sideBar.background" = "#242631";
+          "fontWeight" = "bold";
+        };
+        # HACK: Whitespace on keywords w/o colons is because they occasionally
+        # can appear as part of another word (e.g. DEBUG), and we do not want
+        # highlighting to apply in those situations.
+        "todohighlight.keywords" = [
+          {
+            "text" = "BUG:";
+            "color" = "#ff6c6b";
+          }
+          {
+            "text" = "DEPRECATED:";
+            "color" = "#83898d";
+          }
+          {
+            "text" = "FIXME:";
+            "color" = "#ff6c6b";
+            "backgroundColor" = "none";
+          }
+          {
+            "text" = "HACK:";
+            "color" = "#a9a1e1";
+          }
+          {
+            "text" = "NOTE:";
+            "color" = "#5ea164";
+          }
+          {
+            "text" = "REVIEW:";
+            "color" = "#51afef";
+          }
+          {
+            "text" = "TODO:";
+            "color" = "#ecbe7b";
+            # override default
+            "backgroundColor" = "none";
+          }
+        ];
+        "update.mode" = "manual";
+        "window.menuBarVisibility" = "toggle";
+        # This options seems deprecated
+        #"workbench.activityBar.location" = "hidden";
+        "workbench.colorTheme" = "Tokyo Night";
+        "workbench.colorCustomizations" = {
+          "activityBar.background" = "#242631";
+          "editorGroupHeader.tabsBackground" = "#242631";
+          "statusBar.background" = "#242631";
+          "titleBar.activeBackground" = "#242631";
+          "sideBar.background" = "#242631";
+        };
+        "[haskell]" = {
+          "editor.defaultFormatter" = "haskell.haskell";
+        };
       };
-      "[haskell]" = {
-        "editor.defaultFormatter" = "haskell.haskell";
-      };
+
     };
   };
 }
