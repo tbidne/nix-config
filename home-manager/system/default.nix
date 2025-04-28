b22034a8b3de4c9a51a0d160ad8c450ebbb9b313
Tommy Bidne <tbidne@protonmail.com>
2025-04-28 12:22:29 +1200

    Comment out conky for now

--- home-manager/system/default.nix
+++ home-manager/system/default.nix
@@ -1,7 +1,7 @@
 { inputs }:
 {
   imports = [
-    (import ./gnome/conky.nix { inherit inputs; })
+    #(import ./gnome/conky.nix { inherit inputs; })
     (import ./gnome/default.nix { inherit inputs; })
   ];
 }
