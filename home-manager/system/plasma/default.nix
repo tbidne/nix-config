0948b136bb9fd309bebf1a61064e7c6318525b3c
Tommy Bidne <tbidne@protonmail.com>
2026-01-20 13:49:06 +1300

    Formatting

--- home-manager/system/plasma/default.nix
+++ home-manager/system/plasma/default.nix
@@ -52,8 +52,10 @@
     };
 
     configFile = {
-      "kscreenlockerrc"."Greeter/Wallpaper/org.kde.image/General"."Image" = "file:///home/tommy/Pictures/wallpaper/current";
-      "kscreenlockerrc"."Greeter/Wallpaper/org.kde.image/General"."PreviewImage" = "file:///home/tommy/Pictures/wallpaper/current";
+      "kscreenlockerrc"."Greeter/Wallpaper/org.kde.image/General"."Image" =
+        "file:///home/tommy/Pictures/wallpaper/current";
+      "kscreenlockerrc"."Greeter/Wallpaper/org.kde.image/General"."PreviewImage" =
+        "file:///home/tommy/Pictures/wallpaper/current";
 
       "kwinrc"."Desktops"."Id_1" = "3868ba12-ce3b-4f2a-838a-c79089819633";
       "kwinrc"."Desktops"."Id_2" = "419c8acf-d73b-48a8-a244-a748545c7674";
